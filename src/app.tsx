e3b693ddedd55d40cceba8409d786b72200d9c03
Daniel Troger <daniel@natur-kultur.eu>
2024-01-08 17:41:43 +0100

    Reproduction

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,7 @@
 // @refresh reload
-import { MetaProvider, Title } from "@solidjs/meta";
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start";
-import { Suspense } from "solid-js";
 import "./app.scss";
+import { Counter } from "~/components/Counter";
 
 export default function App() {
-  return (
-    <Router
-      root={(props) => (
-        <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+  return <Counter />;
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,7 +2,16 @@
 import { createSignal } from "solid-js";
 import "./Counter.css";
 
-export default function Counter() {
+const secretSymbol = Symbol();
+
+export const Counter = /*@__PURE__*/ (() => {
+  (ActualCounter as typeof ActualCounter & { [key: symbol]: string })[
+    secretSymbol
+  ] = "ActualCounter";
+  return ActualCounter;
+})();
+
+function ActualCounter() {
   const [count, setCount] = createSignal(0);
   return (
     <button class="increment" onClick={() => setCount(count() + 1)}>
